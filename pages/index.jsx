2a65f5f9f7d7bc9c552e91cef2f646d7d6923342
rodrigo.perez <raapperez@gmail.com>
2018-07-08 10:24:28 -0300

    Config next

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => (
+  <div className="oi">
+    <style jsx>
+      {`
+        .oi {
+          background-color: red;
+        }
+      `}
+    </style>
+    Welcome to next.js!
+    {'oi'}
+  </div>
+);
